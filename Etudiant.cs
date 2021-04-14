86634a00631c95fe413c93fd41bee79bf2e479ff
henrijo225 <82537431+henrijo225@users.noreply.github.com>
2021-04-14 15:58:32 -0400

    Add files via upload

---
+++ Etudiant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace examen
+{
+    public class Etudiant
+    {
+
+        public String nom { get; set; }
+        public String prènom { get; set; }
+        public int nunmeroEtudiant { get; set; }
+
+
+
+    }
+}
